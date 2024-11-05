4deffc71e0dcf2e7f4b123af5068ac6b11f1f81a
Administrator <admin@example.com>
2024-11-05 09:08:45 +0800

    study

--- 001.hello world/main.go
+++ 001.hello world/main.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func main() {
 	// 自动在输出的尾部添加换行符\n,并自动处理参数类型
-	fmt.Println("1.hello world")
+	fmt.Println("001.hello world")
 
 	/*
 		不会自动添加换行符,除非你手动在格式字符串中指定
-		fmt.Printf("%s", "1.hello world")
+		fmt.Printf("%s", "001.hello world")
 	*/
 }
